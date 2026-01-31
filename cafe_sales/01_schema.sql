4dcfe29c448efa9eeee8719f1593dd1c11b2e39b
sarahinesb <sarahinesb@gmail.com>
2026-01-31 18:08:48 +0100

    Create cafe_sales table schema
    Added schema for cafe sales.

--- cafe_sales/01_schema.sql
+++ cafe_sales/01_schema.sql
@@ -1,1 +1,13 @@
+-- Schema for Cafe Sales (cleaned data)
+
+CREATE TABLE cafe_sales (
+  Transaction_ID VARCHAR(50),
+  Item TEXT,
+  Quantity INT,
+  Price_Per_Unit DOUBLE,
+  Total_Spent DOUBLE,
+  Payment_Method TEXT,
+  Location TEXT,
+  Transaction_Date DATE
+);
 
