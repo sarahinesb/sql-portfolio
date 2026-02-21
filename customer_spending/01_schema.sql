fdcd13b7a526075e384933b4485b5f2773cb18c3
sarahinesb <sarahinesb@gmail.com>
2026-02-21 17:08:52 +0100

    Create customer_data table in schema

--- customer_spending/01_schema.sql
+++ customer_spending/01_schema.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE customer_data (
+    name TEXT,
+    age INT,
+    gender TEXT,
+    education TEXT,
+    income INT,
+    country TEXT,
+    purchase_frequency DOUBLE,
+    spending DOUBLE
+);
