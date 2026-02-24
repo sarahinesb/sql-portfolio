2d1082dc27ddaed6af20b88b52b34a9f6537613c
sarahinesb <sarahinesb@gmail.com>
2026-02-24 16:28:00 +0100

    Update 01_schema.sql

--- global-ads-case-study/01_schema.sql
+++ global-ads-case-study/01_schema.sql
@@ -1,1 +1,23 @@
+/*
+=========================================================
+ Global Ads Case Study
+ Schema Definition
+=========================================================
+*/
 
+CREATE TABLE ads_data (
+    date TEXT NULL,
+    platform TEXT NULL,
+    campaign_type TEXT NULL,
+    industry TEXT NULL,
+    country TEXT NULL,
+    impressions INT NULL,
+    clicks INT NULL,
+    CTR DOUBLE NULL,
+    CPC DOUBLE NULL,
+    ad_spend DOUBLE NULL,
+    conversions INT NULL,
+    CPA DOUBLE NULL,
+    revenue DOUBLE NULL,
+    ROAS DOUBLE NULL
+);
